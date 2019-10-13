ce6ec4d8c59e9b9f314a74b30920a09960257d9f
vadym-kriat <kriat.vadym@gmail.com>
2019-10-13 13:42:23 +0300

    implement boot seller agent behaviour and agent GUI

---
+++ src/main/java/ua/nure/lb1/BookSellerAgent.java
@@ -0,0 +1,89 @@
+package ua.nure.lb1;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class BookSellerAgent extends Agent {
+    private Map<String, Integer> catalogue;
+
+    private BookSellerGui bookSellerGui;
+
+    @Override
+    protected void setup() {
+        catalogue = new ConcurrentHashMap<>();
+
+        bookSellerGui = new BookSellerGui(this);
+        bookSellerGui.showFrame();
+
+        addBehaviour(new OfferRequestsServer());
+        addBehaviour(new PurchaseOrdersServer());
+    }
+
+    @Override
+    protected void takeDown() {
+        bookSellerGui.dispose();
+        System.out.println("Seller-agent " + getAID().getName() + " has been terminated.");
+    }
+
+    public void updateCatalogue(final String title, final Integer price) {
+        addBehaviour(new OneShotBehaviour() {
+            @Override
+            public void action() {
+                catalogue.put(title, price);
+            }
+        });
+    }
+
+    private class OfferRequestsServer extends CyclicBehaviour {
+        @Override
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                Integer price = catalogue.get(title);
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(price.toString());
+                } else {
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+    }
+
+    private class PurchaseOrdersServer extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                Integer price = catalogue.remove(title);
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.INFORM);
+                    System.out.println(title + " sold to agent " + msg.getSender().getName());
+                } else {
+                    reply.setPerformative(ACLMessage.FAILURE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+    }
+}
---
+++ src/main/java/ua/nure/lb1/BookSellerGui.java
@@ -0,0 +1,112 @@
+package ua.nure.lb1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+class BookSellerGui extends JFrame {
+    private BookSellerAgent agent;
+    private JTextField titleField;
+    private JTextField priceField;
+    private JButton addButton;
+
+    BookSellerGui(BookSellerAgent agent) {
+        super(agent.getLocalName());
+        this.agent = agent;
+
+        initComponents();
+        initEventListeners();
+    }
+
+    public void showFrame() {
+        pack();
+        centerWindow();
+        super.setVisible(true);
+    }
+
+    private void initComponents() {
+        titleField = new JTextField(15);
+        priceField = new JTextField(15);
+        addButton = new JButton("Add");
+
+        getContentPane().add(getRootPanel(), BorderLayout.CENTER);
+        getContentPane().add(getBottomPanel(), BorderLayout.SOUTH);
+        setResizable(false);
+    }
+
+    private JPanel getRootPanel() {
+        JPanel rootPanel = new JPanel();
+        rootPanel.setLayout(new GridLayout(2, 2));
+
+        rootPanel.add(new JLabel("Book title:"));
+        rootPanel.add(titleField);
+
+        rootPanel.add(new JLabel("Price:"));
+        rootPanel.add(priceField);
+
+        return rootPanel;
+    }
+
+    private JPanel getBottomPanel() {
+        JPanel panel = new JPanel();
+        panel.add(addButton);
+
+        return panel;
+    }
+
+    private void initEventListeners() {
+        addButton.addActionListener(e -> {
+            try {
+                validateFields();
+                String title = titleField.getText().trim();
+                String price = priceField.getText().trim();
+
+                BookSellerGui.this.agent.updateCatalogue(title, Integer.parseInt(price));
+
+                clearFields();
+            } catch (IllegalArgumentException ex) {
+                JOptionPane.showMessageDialog(this, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                agent.doDelete();
+            }
+        });
+    }
+
+    private void validateFields() {
+        String title = titleField.getText().trim();
+        String priceStr = priceField.getText().trim();
+
+        if (title.length() == 0) {
+            throw new IllegalArgumentException("Title is required");
+        }
+        if (title.length() > 250) {
+            throw new IllegalArgumentException("Title is too long");
+        }
+
+        if (priceStr.length() == 0) {
+            throw new IllegalArgumentException("Price is required");
+        }
+        int price = Integer.parseInt(priceStr);
+        if (price < 0) {
+            throw new IllegalArgumentException("Price must be positive");
+        }
+    }
+
+    private void clearFields() {
+        titleField.setText("");
+        priceField.setText("");
+    }
+
+    private void centerWindow() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int centerX = (int) screenSize.getWidth() / 2;
+        int centerY = (int) screenSize.getHeight() / 2;
+        setLocation(centerX - getWidth() / 2, centerY - getHeight() / 2);
+    }
+}
